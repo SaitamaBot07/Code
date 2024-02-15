9fb93ac735c87f7c3cf7bb0e7e30c2de0d1246d7
Samir Londoño <SamirLon0705@gmail.com>
2024-02-15 11:49:58 -0500

    papiiii

--- Desarrollo Web/Actividades/Actividad_04/error.js
+++ Desarrollo Web/Actividades/Actividad_04/error.js
@@ -6,10 +6,16 @@ function valForm1 (form){
     form.preventDefault();
 
     const con = "hola"; 
-    if (con !== "hola") {
+    const pass = form.target[1].value;
+
+    if ( pass !== con) {
         const div = document.createElement('div');
+        div.classList.add('div_error');
         const h1 = document.createElement('h1');
-        
+        h1.textContent = "ERROR";
+        div.appendChild(h1);
+        document.body.appendChild(div);
+
     }
 
 }
