105701e3f0e9ae406d603a74ae1eb8a088ed65d3
Samir Londoño <SamirLon0705@gmail.com>
2024-04-02 10:01:58 -0500

    feat:

--- PI2_LoadBalancer/views_vm.sh
+++ PI2_LoadBalancer/views_vm.sh
@@ -17,7 +17,6 @@ sudo apt-get update
 mkdir /home/docker_files
 cd /home/docker_files
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1_F_7RCno1Uj9_5PYvxyhUHe-e5aP2b8-' -O docker-compose.yml
-sudo docker compose -f /home/docker_files/docker-compose.yml up -d
 sudo touch /home/docker_files/script.sh
 echo "#! /bin/bash" | sudo tee /home/docker_files/script.sh > /dev/null
 echo "sudo docker pull davopaco/views:latest" | sudo tee -a /home/docker_files/script.sh > /dev/null
@@ -25,6 +24,7 @@ echo "sudo docker pull davopaco/nginx_images:latest" | sudo tee -a /home/docker_
 echo "sudo docker compose -f /home/docker_files/docker-compose.yml down" | sudo tee -a /home/docker_files/script.sh > /dev/null
 echo "sudo docker compose -f /home/docker_files/docker-compose.yml up -d" | sudo tee -a /home/docker_files/script.sh > /dev/null
 sudo chmod +x /home/docker_files/script.sh
+sudo /bin/bash /home/docker_files/script.sh
 sudo crontab -l; echo "* 3 * * * /bin/bash /home/docker_files/script.sh" | crontab -
 sudo docker compose -f /home/docker_files/docker-compose.yml up -d
 
